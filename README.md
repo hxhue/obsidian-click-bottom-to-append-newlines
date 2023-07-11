a8808950f6d87f48bc50abff5974aa60ffe9f676
Simon Yu <simon.yx@qq.com>
2023-07-11 22:04:22 +0800

    Remove dom event listeners when plugin is disabled

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Click Bottom to Append Newline
+# Click Bottom to Append New Lines
 
-This plugin allows you to easily add new lines to your document by clicking at the bottom. No configuration is required, but please note that you may need to reopen tabs after disabling the plugin.
+This plugin allows you to easily add new lines to your document by clicking at the bottom. No configuration is required.
 
 ## How does it work?
 
-This plugin registers a DOM event to check if a click occurs at the bottom of the page. If the click position goes beyond the last non-empty paragraph and exceeds a distance of two lines (there should be an empty line between adjacent paragraphs in Markdown), the plugin will insert additional empty lines at the end of the document and move the cursor there.
+This plugin registers DOM events to check if a click occurs at the bottom of the page. If the click position goes beyond the last non-empty paragraph and exceeds a distance of two lines (because there should be an empty line between adjacent paragraphs in Markdown), the plugin will insert additional empty lines at the end of the document and move the cursor there.
--- main.ts
+++ main.ts
@@ -1,111 +1,142 @@
 import { MarkdownView, Notice, Plugin } from "obsidian";
 import { setTimeout } from "timers";
 
-export default class MyPlugin extends Plugin {
-	onclickAttribute = this.manifest.id + "-onclick-attribute";
+export default class AppendNewlinesPlugin extends Plugin {
+	readonly tagAttribute = this.manifest.id + "-tag-attribute";
+	readonly scrollerSelector = "div.cm-scroller";
+	readonly lineWrappingSelector = "div.cm-content.cm-lineWrapping";
+	listenerMap = new WeakMap<HTMLElement, { click: EventListener; mousedown: EventListener }>();
 
-	async onload() {
-		this.addMyListener();
-		this.registerEvent(this.app.workspace.on("active-leaf-change", this.addMyListener));
-	}
+	// Add event listeners to the markdown view
+	addEventListeners(markdownView: MarkdownView) {
+		const editor = markdownView.editor;
+		const scroller = document.querySelector(this.scrollerSelector) as HTMLElement;
+		if (!scroller) {
+			new Notice(this.manifest.id + ": cannot get scroller view!");
+			return;
+		}
+		// Listeners exist.
+		if (this.listenerMap.get(scroller) !== undefined) {
+			return;
+		}
 
-	addMyListener = () => {
-		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		if (activeView && !activeView.containerEl.getAttribute(this.onclickAttribute)) {
-			const editor = activeView.editor;
-			const scroller = document.querySelector("div.cm-scroller") as HTMLElement;
-
-			if (!scroller) {
-				new Notice(this.manifest.id + ": cannot get scroller view!");
-				return;
+		// It may seem inefficient to check this both in 'mousedown' and 'click'
+		// events, but when 'click' is fired the mouse position may have changed.
+		const linesToAppend = (event: MouseEvent) => {
+			if (event.button !== 0) {
+				return 0; // Not left click
 			}
 
-			// It may seem inefficient to check this both in 'mousedown' and 'click'
-			// events, but when 'click' is fired the mouse position may have changed.
-			const linesToAppend = (event: MouseEvent) => {
-				if (event.button !== 0) {
-					return 0; // Not left click
+			const distance = (() => {
+				let elem = event.target as HTMLElement;
+				let mouseY = event.offsetY;
+
+				// A child receives the event
+				while (elem !== scroller) {
+					mouseY += elem.offsetTop;
+					elem = elem.offsetParent as HTMLElement;
 				}
 
-				const distance = (() => {
-					let elem = event.target as HTMLElement;
-					let mouseY = event.offsetY;
-
-					// A child receives the event
-					while (elem !== scroller) {
-						mouseY += elem.offsetTop;
-						elem = elem.offsetParent as HTMLElement;
-					}
-
-					// Get the last element: possibly a paragraph or a picture
-					elem = document.querySelector("div.cm-content.cm-lineWrapping")
-						?.lastElementChild as HTMLElement;
-					let contentY = elem.offsetTop + elem.offsetHeight;
-					while (elem.offsetParent !== scroller) {
-						elem = elem.offsetParent as HTMLElement;
-						contentY += elem.offsetTop + elem.offsetHeight;
-					}
-					return mouseY - contentY;
-				})();
-
-				if (distance < 0) {
-					return 0; // Click didn't happen at the bottom
-				}
-
-				const lastLine = editor.lastLine();
-				const lastLineIsBlank = editor.getLine(lastLine).trim() === "";
-
-				if (lastLineIsBlank && lastLine == 0) {
-					return 0; // The document is one blank line
-				}
-
-				if (lastLineIsBlank && editor.getLine(lastLine - 1).trim() === "") {
-					return 0; // The document ends with 2 blank lines
-				}
-
-				const lineHeightInPx = (() => {
-					const height =
-						document.defaultView?.getComputedStyle(scroller)?.lineHeight ?? "24px";
-					// Trailing 'px' will be ignored by Number.parseFloat
-					return Number.parseFloat(height);
-				})();
-				const threshold = lastLineIsBlank ? 0 : lineHeightInPx;
-				const newlines = lastLineIsBlank ? 1 : 2;
-				return distance > threshold ? newlines : 0;
-			};
-
-			scroller.addEventListener("mousedown", (event: MouseEvent) => {
-				const lines = linesToAppend(event);
-				if (lines <= 0) {
-					return;
-				}
-				let paragraph = document.querySelector("div.cm-content.cm-lineWrapping")
+				// Get the last element: possibly a paragraph or a picture
+				elem = document.querySelector("div.cm-content.cm-lineWrapping")
 					?.lastElementChild as HTMLElement;
-				while (paragraph && !paragraph.className.includes("cm-line")) {
-					paragraph = paragraph.previousElementSibling as HTMLElement;
+				let contentY = elem.offsetTop + elem.offsetHeight;
+				while (elem.offsetParent !== scroller) {
+					elem = elem.offsetParent as HTMLElement;
+					contentY += elem.offsetTop + elem.offsetHeight;
 				}
-				if (paragraph) {
-					const caretColor = paragraph.style.caretColor;
-					paragraph.style.caretColor = "transparent";
-					setTimeout(() => {
-						paragraph.style.caretColor = caretColor;
-					}, 100);
-				}
-			});
+				return mouseY - contentY;
+			})();
 
-			// Appending lines directly in 'mousedown' listener has some deficits:
-			// - <img> element doesn't adjust instantly when two consecutive lines are appended.
-			// - Lines may be selected when clicks happen too fast.
-			scroller.addEventListener("click", (event: MouseEvent) => {
-				const lines = linesToAppend(event); // lines may be 0
-				for (let i = 0; i < lines; ++i) {
-					editor.exec("newlineAndIndent");
-				}
-			});
+			if (distance < 0) {
+				return 0; // Click didn't happen at the bottom
+			}
 
-			activeView.containerEl.setAttribute(this.onclickAttribute, "true");
-		}
+			const lastLine = editor.lastLine();
+			const lastLineIsBlank = editor.getLine(lastLine).trim() === "";
+
+			if (lastLineIsBlank && lastLine == 0) {
+				return 0; // The document is one blank line
+			}
+
+			if (lastLineIsBlank && editor.getLine(lastLine - 1).trim() === "") {
+				return 0; // The document ends with 2 blank lines
+			}
+
+			const lineHeightInPx = (() => {
+				const height =
+					document.defaultView?.getComputedStyle(scroller)?.lineHeight ?? "24px";
+				// Trailing 'px' will be ignored by Number.parseFloat
+				return Number.parseFloat(height);
+			})();
+			const threshold = lastLineIsBlank ? 0 : lineHeightInPx;
+			const newlines = lastLineIsBlank ? 1 : 2;
+			return distance > threshold ? newlines : 0;
+		};
+
+		// Disable cursor blink until two `editor.exec("newlineAndIndent")` complete
+		const mousedown = (event: MouseEvent) => {
+			const lines = linesToAppend(event);
+			if (lines <= 0) {
+				return;
+			}
+			let paragraph = document.querySelector(this.lineWrappingSelector)
+				?.lastElementChild as HTMLElement;
+			while (paragraph && !paragraph.className.includes("cm-line")) {
+				paragraph = paragraph.previousElementSibling as HTMLElement;
+			}
+			if (paragraph) {
+				const caretColor = paragraph.style.caretColor;
+				paragraph.style.caretColor = "transparent";
+				setTimeout(() => {
+					paragraph.style.caretColor = caretColor;
+				}, 100);
+			}
+		};
+
+		// Appending lines directly in 'mousedown' listener has those deficits which are the
+		// reasons why we register 'click' listener:
+		// - <img> elements don't adjust instantly when two consecutive lines are appended.
+		// - Lines may be selected when clicks happen too fast.
+		const click = (event: MouseEvent) => {
+			const lines = linesToAppend(event); // lines may be 0
+			for (let i = 0; i < lines; ++i) {
+				editor.exec("newlineAndIndent");
+			}
+		};
+
+		scroller.addEventListener("mousedown", mousedown);
+		scroller.addEventListener("click", click);
+		this.listenerMap.set(scroller, { click, mousedown });
 	};
 
-	onunload() {}
+	async onload() {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (view) {
+			this.addEventListeners(view);
+		}
+		this.registerEvent(this.app.workspace.on("active-leaf-change", (leaf) => {
+			if (leaf?.view.getViewType() === 'markdown') {
+				this.addEventListeners(leaf.view as MarkdownView)
+			}
+		}));
+	}
+
+	onunload() {
+		app.workspace.iterateAllLeaves((leaf) => {
+			if (leaf.view.getViewType() !== "markdown") {
+				return;
+			}
+			const scroller = leaf.view.containerEl.querySelector(
+				this.scrollerSelector
+			) as HTMLElement;
+			const listeners = this.listenerMap.get(scroller);
+			if (listeners === undefined) {
+				return;
+			}
+			const { click, mousedown } = listeners;
+			scroller.removeEventListener("click", click);
+			scroller.removeEventListener("mousedown", mousedown);
+		});
+	}
 }
